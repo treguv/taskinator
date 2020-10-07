70505e522ecf7ba3bed613ab55baa63da603382b
treguv <vladislavtregubov00@gmail.com>
2020-10-06 17:56:30 -0700

    added click to submit and submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,16 @@ In this case we are adding event listener to button, that listens for the click,
 //Selects button and assigns to var
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
+
 //Add event listener to button
 
 var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+  event.preventDefault();
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+}
 
-buttonE1.addEventListener("click",  createTaskHandler);
+formEl.addEventListener("submit",  createTaskHandler);
