cf61bda092527abd57c1490c64dc965599a4d0cd
treguv <vladislavtregubov00@gmail.com>
2020-10-06 18:23:07 -0700

    Added secondary info to the task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
-
 /*
 An event is when something happens or is interacted with
 eventListener is somethign that waits for that evert
@@ -18,8 +16,15 @@ var formEl = document.querySelector("#task-form");
 var createTaskHandler = function() {
   event.preventDefault();
   var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  listItemEl.className="task-item";
+  //Make div container
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput= document.querySelector("select[name='task-type']").value;
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
   tasksToDoEl.appendChild(listItemEl);
 }
 
